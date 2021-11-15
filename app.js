2000358a2498a6eef1e29e5ea587f0164499a0ce
ae87hw <ae87@hw.ac.uk>
2021-11-15 13:18:58 +0400

    hmmm

--- app.js
+++ app.js
@@ -21,3 +21,22 @@ app.set('view engine', 'ejs');
 app.get('/', (req, res) => {
     res.render('index'); //no need for ejs extension 
 });
+
+//route for contacts
+app.get('/contacts', (req, res) => {
+    res.render('contacts'); 
+});
+
+//route for username in login.ejs file
+app.get('/register', (req, res) => {
+    res.render('register');
+});
+
+//route for password in login.ejs file
+app.get('/login', (req, res) => {
+    res.render('login'); 
+});
+
+//pass requests to the router middleware
+// const router = require('./routes/apis'); 
+// app.use(router);
